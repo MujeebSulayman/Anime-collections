3daffeb5a1f96f9f236af6049ee309214c9d45d1
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2023-12-23 14:16:48 +0100

    Data and footer component gone for good

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,14 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 
 import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
+
 
 import "./globals.css";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Anime Vault",
+  title: "Anime Collection",
   description: "Your favorite anime, all in one place.",
 };
 
@@ -24,7 +24,7 @@ export default function RootLayout({
         <main className="max-w-7xl mx-auto bg-[#0F1117]">
           <Hero />
           {children}
-          <Footer />
+
         </main>
       </body>
     </html>
